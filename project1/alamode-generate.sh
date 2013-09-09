2266c9685db04219e3f71371ddb3beaaac8a2c9b
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-09 17:35:49 -0600

    adding in home disk space metrics

--- project1/alamode-generate.sh
+++ project1/alamode-generate.sh
@@ -5,4 +5,5 @@
 # it is running on. For example, numebr of processes in a certain state,
 # total number of users logged in, processor usage, disk usuage, etc
 
-echo "Current number of users logged in: $(who | wc -l)"   >>  "/tmp/`hostname`"
+echo "Current number of users logged in: $(who | wc -l)"   >  "/tmp/`hostname`"
+echo "You currently are taking up: $(du -ch | grep total) space in your home directory" >> "/tmp/`hostname`"
