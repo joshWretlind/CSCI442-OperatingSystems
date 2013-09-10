11a17b68718571c1ccc9943fbdc58ce7ef4592c1
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-10 15:44:41 -0600

    adding in average load

--- project1/alamode-generate.sh
+++ project1/alamode-generate.sh
@@ -6,6 +6,6 @@
 # total number of users logged in, processor usage, disk usuage, etc
 
 echo "Current number of users logged in: $(who | wc -l)"   >  "/tmp/`hostname`"
-echo "You currently are taking up: $(du -ch | grep total | sed -e "s/\s*total\s*//") amount of space in your home directory" >> "/tmp/`hostname`"
+echo "You currently are taking up this amount of space in home: $(du -ch ~ | grep total | sed -e "s/\s*total\s*//")" >> "/tmp/`hostname`"
 echo "Total amount of ram: $(cat /proc/meminfo | grep MemTotal | sed -e " s/MemTotal:\s*//g")" >> "/tmp/`hostname`"
- 
+echo "1 minute, 5 minute, 15 minute average load: $(cat /proc/loadavg | sed -e "s/[0-9]\/*//g")" >> "/tmp/`hostname`
