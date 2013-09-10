0cf4c4ae382919cfaa11dfaeb0949942c927f5cf
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-10 16:09:16 -0600

    fixing load regex to match correctly

--- project1/alamode-generate.sh
+++ project1/alamode-generate.sh
@@ -8,4 +8,4 @@
 echo "Current number of users logged in: $(who | wc -l)"   >  "/tmp/`hostname`"
 echo "You currently are taking up this amount of space in home: $(du -ch ~ | grep total | sed -e "s/\s*total\s*//")" >> "/tmp/`hostname`"
 echo "Total amount of ram: $(cat /proc/meminfo | grep MemTotal | sed -e " s/MemTotal:\s*//g")" >> "/tmp/`hostname`"
-echo "1 minute, 5 minute, 15 minute average load: $(cat /proc/loadavg | sed -e "s/[0-9]\/*//g")" >> "/tmp/`hostname`"
+echo "1 minute, 5 minute, 15 minute average load: $(cat /proc/loadavg | sed -e "s/[0-9]\/+//g")" >> "/tmp/`hostname`"
