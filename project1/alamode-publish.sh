f17c5ca407ae58fb7eb672b7badf35ee0ed966a3
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-17 01:30:34 -0600

    cleaning up output as well as fixing a weird issue with having then in the ls list

--- project1/alamode-publish.sh
+++ project1/alamode-publish.sh
@@ -38,7 +38,6 @@ while getopts ":d:s:h" OPTIONS
     shift 2
 done
 
-echo $1
 if [ ! -z $INPUT_DIRECTORY ]
 then
     if [ ! -z $1 ]
@@ -63,23 +62,25 @@ if [ ! -z $INPUT_DIRECTORY ]
 then
     directory=$INPUT_DIRECTORY
 fi
-echo $directory
+
 final_host_data=""
 ls $directory
 for file in $(ls $directory) then
 do
-    inner=$(cat templateInsideMachine.bstl)
-    inner=$(echo "$inner" | sed -e "s/@HOSTNAME/$file/g")
-    inner=$(echo "$inner" | sed -e "s/@USERSLOGGEDIN/$(cat $directory/$file | grep users | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@HOMESPACE/$(cat $directory/$file | grep home | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@TOTALTASKCOUNT/$(cat $directory/$file | grep Tasks | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@RUNNINGTASKCOUNT/$(cat $directory/$file | grep Running | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@SLEEPINGTASKCOUNT/$(cat $directory/$file | grep sleeping | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@STOPPEDTASKCOUNT/$(cat $directory/$file | grep stopped | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@ZOMBIETASKCOUNT/$(cat $directory/$file | grep zombie | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@TOTALRAMAVAILIBLE/$(cat $directory/$file | grep ram | sed -e 's/[^0-9]//g')/g" )
-    inner=$(echo "$inner" | sed -e "s/@1MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//2' | sed -e 's/[0-9]\.[0-9][0-9]//2' )/g" )
-    inner=$(echo "$inner" | sed -e "s/@5MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//2' )/g" )
-    inner=$(echo "$inner" | sed -e "s/@15MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//1' )/g" )
-    echo $inner
+    if [ ! $directory -eq "then"]
+    then
+        inner=$(cat templateInsideMachine.bstl)
+        inner=$(echo "$inner" | sed -e "s/@HOSTNAME/$file/g")
+        inner=$(echo "$inner" | sed -e "s/@USERSLOGGEDIN/$(cat $directory/$file | grep users | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@HOMESPACE/$(cat $directory/$file | grep home | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@TOTALTASKCOUNT/$(cat $directory/$file | grep Tasks | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@RUNNINGTASKCOUNT/$(cat $directory/$file | grep Running | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@SLEEPINGTASKCOUNT/$(cat $directory/$file | grep sleeping | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@STOPPEDTASKCOUNT/$(cat $directory/$file | grep stopped | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@ZOMBIETASKCOUNT/$(cat $directory/$file | grep zombie | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@TOTALRAMAVAILIBLE/$(cat $directory/$file | grep ram | sed -e 's/[^0-9]//g')/g" )
+        inner=$(echo "$inner" | sed -e "s/@1MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//2' | sed -e 's/[0-9]\.[0-9][0-9]//2' )/g" )
+        inner=$(echo "$inner" | sed -e "s/@5MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//2' )/g" )
+        inner=$(echo "$inner" | sed -e "s/@15MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//1' )/g" )
+    fi
 done
