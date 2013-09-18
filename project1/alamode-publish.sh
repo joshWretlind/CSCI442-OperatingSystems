2362b8fb6a2174c3565d975548e4153dbecf1ced
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-09-17 22:57:09 -0600

    chaning coloring if statements so that hopefully they'll work

--- project1/alamode-publish.sh
+++ project1/alamode-publish.sh
@@ -80,12 +80,12 @@ do
     inner=$(echo "$inner" | sed -e "s/@5MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//2' )/g" )
     inner=$(echo "$inner" | sed -e "s/@15MINUTELOADAVERAGE/$(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//1' )/g" )
 	loadavg=$(echo $(cat $directory/$file | grep minute | sed -e 's/.*://g' | sed -e 's/[0-9]\.[0-9][0-9]//1' | sed -e 's/[0-9]\.[0-9][0-9]//1' ) | sed -e 's/\s//g') 
-	if [ $(echo "$loadavg > 0.5" | bc) ]
+	if [ "$(echo '$loadavg > 0.5' | bc)" ]
 	then
 		inner=$(echo "$inner" | sed -e 's/@TITLECOLOR/style=\"color:red\"/g')
 	fi
     
-    if [ $(echo "$loadavg > 0.10" | bc) ]
+    if [ "$(echo '$loadavg > 0.10' | bc)" ]
 	then
 		inner=$(echo "$inner" | sed -e 's/@TITLECOLOR/style=\"color:orange\"/g')
 	fi
