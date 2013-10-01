cead3406ad3b83e9bfa3b0eea52b7cb0cf954274
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-01 17:24:56 -0600

    fixing syntax issues with last commit

--- project2/builtins.cpp
+++ project2/builtins.cpp
@@ -97,8 +97,8 @@ int com_exit(vector<string>& tokens) {
 
 
 int com_history(vector<string>& tokens) {
-  for(HIST_ENTRY *comm = previous_history(); comm != NULL;*comm = next_history()){
-    cout << comm.line << endl;
+  for(HIST_ENTRY *comm = previous_history(); comm != NULL; comm = previous_history()){
+    cout << comm->line << endl;
   }
   return 0;
 }
