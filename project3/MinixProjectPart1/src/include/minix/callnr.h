9bf45c47bb2bc319077af4224fed7af6720fa0d1
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-30 21:15:06 -0600

    fixing whitespace, adding in call number for the GETPROCTABLE

--- project3/MinixProjectPart1/src/include/minix/callnr.h
+++ project3/MinixProjectPart1/src/include/minix/callnr.h
@@ -17,10 +17,10 @@
 #define MKNOD		  14 
 #define CHMOD		  15 
 #define CHOWN		  16 
-#define BRK		  17
+#define BRK		      17
 #define PREV_STAT	  18
 #define LSEEK		  19
-#define MINIX_GETPID	  20
+#define MINIX_GETPID  20
 #define MOUNT		  21 
 #define UMOUNT		  22 
 #define SETUID		  23
@@ -40,7 +40,7 @@
 #define RENAME		  38
 #define MKDIR		  39
 #define RMDIR		  40
-#define DUP		  41 
+#define DUP		      41 
 #define PIPE		  42 
 #define TIMES		  43
 #define SYMLINK		  45
@@ -65,8 +65,8 @@
 #define ITIMER		  64
 #define GETGROUPS_O	  65
 #define SETGROUPS_O	  66
-#define GETMCONTEXT       67
-#define SETMCONTEXT       68
+#define GETMCONTEXT   67
+#define SETMCONTEXT   68
 
 /* Posix signal handling. */
 #define SIGACTION	  71
@@ -83,36 +83,37 @@
 #define FSTATFS	 	  82	/* to VFS */
 #define STATVFS 	  83	/* to VFS */
 #define FSTATVFS 	  84	/* to VFS */
-#define SELECT            85	/* to VFS */
-#define FCHDIR            86	/* to VFS */
-#define FSYNC             87	/* to VFS */
-#define GETPRIORITY       88	/* to PM */
-#define SETPRIORITY       89	/* to PM */
-#define GETTIMEOFDAY      90	/* to PM */
+#define SELECT        85	/* to VFS */
+#define FCHDIR        86	/* to VFS */
+#define FSYNC         87	/* to VFS */
+#define GETPRIORITY   88	/* to PM */
+#define SETPRIORITY   89	/* to PM */
+#define GETTIMEOFDAY  90	/* to PM */
 #define SETEUID		  91	/* to PM */
 #define SETEGID		  92	/* to PM */
 #define TRUNCATE	  93	/* to VFS */
 #define FTRUNCATE	  94	/* to VFS */
 #define FCHMOD		  95	/* to VFS */
 #define FCHOWN		  96	/* to VFS */
-#define SPROF             98    /* to PM */
-#define CPROF             99    /* to PM */
+#define SPROF         98    /* to PM */
+#define CPROF         99    /* to PM */
 
 /* Calls provided by PM and FS that are not part of the API */
-#define PM_NEWEXEC	100	/* from VFS or RS to PM: new exec */
-#define SRV_FORK  	101	/* to PM: special fork call for RS */
-#define EXEC_RESTART	102	/* to PM: final part of exec for RS */
-#define GETPROCNR	104	/* to PM */
-#define ISSETUGID	106	/* to PM: ask if process is tainted */
-#define GETEPINFO_O	107	/* to PM: get pid/uid/gid of an endpoint */
-#define SRV_KILL  	111	/* to PM: special kill call for RS */
+#define PM_NEWEXEC	  100	/* from VFS or RS to PM: new exec */
+#define SRV_FORK  	  101	/* to PM: special fork call for RS */
+#define EXEC_RESTART  102	/* to PM: final part of exec for RS */
+#define GETPROCNR	  104	/* to PM */
+#define ISSETUGID	  106	/* to PM: ask if process is tainted */
+#define GETEPINFO_O	  107	/* to PM: get pid/uid/gid of an endpoint */
+#define SRV_KILL  	  111	/* to PM: special kill call for RS */
 
-#define GCOV_FLUSH	112	/* flush gcov data from server to gcov files */
+#define GCOV_FLUSH	  112	/* flush gcov data from server to gcov files */
 
-#define PM_GETSID	113	/* PM getsid() */
+#define PM_GETSID	  113	/* PM getsid() */
 
-#define TASK_REPLY	121	/* to VFS: reply code from drivers, not 
+#define TASK_REPLY	  121	/* to VFS: reply code from drivers, not 
 				 * really a standalone call.
 				 */
-#define MAPDRIVER      122     /* to VFS, map a device */
+#define MAPDRIVER     122   /* to VFS, map a device */
 
+#define GETPROCTABLE  123    /* to get the process table */
