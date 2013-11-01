06ac2dcbf12447732ee85717a3be57ac5f388be4
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-10-31 20:43:53 -0600

    updating files with files from team repo

--- project3/Minix/OSPEX/student.h
+++ project3/Minix/OSPEX/student.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <minix/pi.h>
+#include "lib.h"
 #include "com.h"
 #include "glo.h"
 #include "procs.h"
--- project3/Minix/lib/libsys/printmessage.c
+++ project3/Minix/lib/libsys/printmessage.c
@@ -1,9 +1,7 @@
 #include "lib.h"
 #include "unistd.h"
-#include "printmessage.h"
 
-
-public int printmessage(void) {
+int printmessage(void) {
 	message m;
 	return(_syscall(PM_PROC_NR, 69, &m));
 }
