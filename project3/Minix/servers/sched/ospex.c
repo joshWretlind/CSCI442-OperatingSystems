caf557aed5f6d9548f9969b3311ec11da92907d9
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-05 21:24:54 -0700

    syncing repo with team

--- project3/Minix/servers/sched/ospex.c
+++ project3/Minix/servers/sched/ospex.c
@@ -11,21 +11,32 @@ void OSSendPtab(void) {
 
 		// After this call, processes holds the process table
 		sys_getproctab(&processes);
+		printf("System call complete");
 
 		/* Replace struct pi pInfo[i][] = NULL with process table information from the scheduler*/
 		for ( int i = 0; i < TOTALPROCS; i++ ) {
 			strcpy(process_info[call_count][i].p_name, processes[i].p_name);
+			printf("p_name copied");
 			process_info[call_count][i].p_endpoint = processes[i].p_endpoint;
+			printf("p_endpoint copied");
 			process_info[call_count][i].p_priority = processes[i].p_priority;
+			printf("p_priority copied");
 			process_info[call_count][i].p_cpu_time_left = processes[i].p_cpu_time_left;
+			printf("p_cpu_time_left copied");
 			process_info[call_count][i].p_rts_flags = processes[i].p_rts_flags;
+			printf("p_rts_flags copied");
 
 			strcpy(process_info[call_count][i].p_nextready, (const char*) processes[i].p_nextready);
+			printf("p_nextready copied");
 			process_info[call_count][i].p_nextready_endpoint = processes[i].p_nextready->p_endpoint;
-			
+			printf("p_nextready_endpoint copied");
+
 			process_info[call_count][i].p_user_time = processes[i].p_user_time;
+			printf("p_user_time copied");
 			process_info[call_count][i].p_sys_time = processes[i].p_sys_time;
+			printf("p_sys_time copied");
 			process_info[call_count][i].p_cycles = processes[i].p_cycles;
+			printf("p_cycles copied");
 
 			// Assign individually
 			process_info[call_count][i].p_times.enter_queue = processes[i].p_accounting.enter_queue;
