a7c9e7d692f241bc1e1c86735e5c0d33f15da090
Josh Wretlind <jwretlin@mymail.mines.edu>
2013-11-25 06:11:17 -0700

    still trying printing out of free_pages_bitmap

--- project4/Minix/servers/vm/alloc.c
+++ project4/Minix/servers/vm/alloc.c
@@ -265,8 +265,10 @@ static phys_bytes alloc_pages(int pages, int memflags)
 	/* remember for next time */
 	lastscan = mem_low;
 	
-
-	
+	for(int i = 0; i < BITMAP_CHUNKS; i++){
+		printf("%llu", free_pages_bitmap[i]);
+	}
+	printf("\n");	
 	for(i = mem; i < mem + pages; i++) {
 		UNSET_BIT(free_pages_bitmap, i);
 	}
